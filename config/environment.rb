e4c1155067aa8db8cb004ebc12c2a6125b90dd2a
alexbarron <alexbarron@gmail.com>
2016-02-06 16:16:19 -0800

    Display team roster in CLI

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 require 'pry'
 require 'nokogiri'
 require 'open-uri'
+require 'terminal-table'
 
 
 require_relative '../lib/nba_stats_cli'
--- lib/nba_stats_cli.rb
+++ lib/nba_stats_cli.rb
@@ -11,10 +11,22 @@ class NbaStatsCli
     Team.all.each do |team|
       puts team.name
     end
+    puts "Enter a team name to see their roster: "
+    requested_team = gets.strip
+    team = Team.all.detect {|team| team.name == requested_team}
+    team.add_players
+    puts team.name + " roster:"
+    rows = [["Number", "Name", "Position", "Height", "Experience"]]
+    team.players.each do |player|
+      rows << [player.number, player.name, player.position, player.height, player.experience]
+    end
+    table = Terminal::Table.new rows: rows
+    puts table
   end
 
   def self.make_teams
     teams_array = Scraper.get_teams
     Team.create_from_collection(teams_array)
   end
+
 end
